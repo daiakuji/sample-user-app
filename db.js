cebac3fa38d0825eeb8f033b47030210f1246bae
daiakuji <daiakuji@gmail.com>
2015-08-28 15:15:48 -0400

    Initial Commit

---
+++ db.js
@@ -0,0 +1,89 @@
+// Loading and initializing the library without options;
+// See also: https://github.com/vitaly-t/pg-promise#initialization-options
+var pgp = require('pg-promise')(/*options*/);
+var promise = require('bluebird'); 
+var config = require('./db_config');
+
+// Database connection details;
+var cn = {
+    host: config.host,  // server name or IP address;
+    port: config.port,         // default port;
+    database: config.database, // local database name for testing;
+    user: config.user,    // user name;
+    password: config.password, // add password, if needed;
+	poolSize: config.poolSize,	//number of connections to use in connection pool
+	poolIdleTimeout: config.poolIdleTimeout,   //max milliseconds a client can go unused before it is removed
+	reapIntervalMillis: config.reapIntervalMillis	  //frequeny to check for idle clients within the client pool
+
+};
+// You can check for all default values in:
+// https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
+
+var db = pgp(cn); // database instance;
+
+var count = 1;
+
+var	getUserList = function(success,fail){
+db.query("select * from users where active=$1", true)
+    .then(success,fail);
+};
+
+var getUser = function(id, success, fail){
+db.query("select * from users where id=$1", id)
+    .then(success,fail)
+};
+
+var addUser = function(body, success, fail){
+	db.one("insert into users(login, active) values($1, $2) returning id",
+		[body.login, true])
+		.then(success,fail);
+};
+
+var updateUser = function(id, body, success, fail){
+	db.tx(function (t) {
+    return promise.all([
+        t.none("update users set login=$1 where id=$2",
+            [body.login, id])
+		//,
+        // Set an audit row
+		//t.none("insert into audit(status, id) values($1, $2)",
+        //    ['active', 123])
+    ]);
+})
+    .then(success,fail);
+};
+
+var deleteUser = function(id, body, success, fail){
+	db.tx(function (t) {
+    // t = this
+    return promise.all([
+        t.none("DELETE FROM users WHERE id = $1", id)
+		//,
+		// Set an audit row
+        //t.none("insert into audit(status, id) values($1, $2)",
+		//	['active', 123])
+    ]);
+})
+    .then(function (data) {
+        // success;
+    }, function (reason) {
+        // error;
+    });
+};
+
+var increment = function() {
+    count++;
+};
+
+var	getCount = function() {
+    return count;
+}	
+
+module.exports = {
+	db:	db,
+	getUserList:	getUserList,
+	getUser:	getUser,
+	addUser:	addUser,
+	increment:	increment,
+	getCount:	getCount
+};
---
+++ routes/users.js
@@ -0,0 +1,63 @@
+//Load Dependencies
+var express = require('express');
+var router = express.Router();
+var dbreq = require('../db');
+
+/* GET users listing. */
+router.route("/list")
+	.get(function(req, res) {
+	dbreq.getUserList(function(data) {
+		res.render('userlist', {
+			"userlist" : data
+		});
+	}, function(reason) {
+		res.json({ errors: [reason] }).status(503);
+	});
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User' });
+});
+
+/* GET user by ID. Note: Causes a 503 error if moved */
+router.route("/:id")
+	.get(function(req, res) {
+		dbreq.getUser(req.params.id,function(data) {
+			res.render('user', {
+				"id"	:	data[0].id,
+				"login"	:	data[0].login
+			});
+		}, function(reason) {
+			res.json({ errors: [reason] }).status(503);
+		});
+	})
+	/* PUT to Update User Service*/
+	.put(function(req, res) {
+		dbreq.updateUser(req.params.id, req.body, function(data) {
+			res.redirect("/users/"+data.id);
+		}, function(reason) {
+			res.json({ errors: [reason] }).status(503);
+		});
+	})
+	/* DELETE to Delete User Service*/
+	.delete(function(req, res) {
+		dbreq.deleteUser(req.params.id, function() {
+			res.redirect("/list");
+		}, function(reason) {
+			res.json({ errors: [reason] }).status(503);
+		});
+	});		
+  
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+	dbreq.addUser(req.body,function(data) {
+		res.redirect("/users/"+data.id);
+	}, function (reason) {
+		res.json({ errors: [reason] }).status(503);	
+	});
+});
+
+
+
+module.exports = router;
